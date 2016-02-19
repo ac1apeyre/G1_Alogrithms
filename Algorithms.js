ed379726236232d8c259e012e32f93e25898097a
ac1apeyre <lapeyre.ac@gmail.com>
2016-02-19 12:55:27 -0500

    Update Algorithms.js

--- Algorithms.js
+++ Algorithms.js
@@ -120,7 +120,8 @@ function addImageSourcesFunctions(scene) {
 
 		//TODO: complete the recursive scene graph traversal
 		// Start off recursion by calling it with scene and the identity matrix: func(scene, mat4.create())
-  		// for each node in the scene graph
+  		// for each node in the scene graph?
+  		
   			for c in node.children:// for each child in node.children:
   				if (mesh in node){ // check if node is dummy: if not dummy node, it will have a mesh object
   					var mesh = node.mesh; // access the mesh object
@@ -162,88 +163,47 @@ function addImageSourcesFunctions(scene) {
 
 
 	//Purpose: Based on the extracted image sources, trace back paths from the
-    
 	//receiver to the source, checking to make sure there are no occlusions
-    
 	//along the way.  Remember, you're always starting by tracing a path from
-    
 	//the receiver to the image, and then from the intersection point with
-    
 	//that image's corresponding face to the image's parent, and so on
-    
 	//all the way until you get back to the original source.
-    
-    
 	//Fill in the array scene.paths, where each element of the array is itself
-    
 	//an array of objects describing vertices along the path, starting
-    
 	//with the receiver and ending with the source.  Each object in each path
-    
 	//array should contain a field "pos" which describes the position, as well
-    
 	//as an element "rcoeff" which stores the reflection coefficient at that
     
 	//part of the path, which will be used to compute decays in "computeInpulseResponse()"
     
 	//Don't forget the direct path from source to receiver!
     
-
-
-	/**PATH EXTRACTION**
+    
 	scene.extractPaths = function() {
-        
 		scene.paths = [];
         
-        
 		//TODO: Finish this. Extract the rest of the paths by backtracing from
-        
 		//the image sources you calculated.  Return an array of arrays in
-        
 		//scene.paths.  Recursion is highly recommended
-      
 		//Each path should start at the receiver and end at the source
-        
 		//(or vice versa), so scene.receiver should be the first element 
-        
 		//and scene.source should be the last element of every array in 
-        
 		//scene.paths
-    
 	}
-
-
-    
-    
     
 	//Inputs: Fs: Sampling rate (samples per second)
-    
-	
-
-	//**IMPULSE RESPONSE GENERATION**
 	scene.computeImpulseResponse = function(Fs) {
         
 		var SVel = 340;	//Sound travels at 340 meters/second
-        
-
 		//TODO: Finish this.  Be sure to scale each bounce by 1/(1+r^p), 
-        
 		//where r is the length of the line segment of that bounce in meters
-        
 		//and p is some integer less than 1 (make it smaller if you want the 
-        
 		//paths to attenuate less and to be more echo-y as they propagate)
-        
 		//Also be sure to scale by the reflection coefficient of each material
-        
 		//bounce (you should have stored this in extractPaths() if you followed
-        
 		//those directions).  Use some form of interpolation to spread an impulse
-        
 		//which doesn't fall directly in a bin to nearby bins
-        
 		//Save the result into the array scene.impulseResp[]
-    
 	}
 
 }
