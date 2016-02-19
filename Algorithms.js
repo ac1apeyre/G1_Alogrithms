8d8254e6ea2e52556d2b894ce5588a5b6430c9e7
ac1apeyre <lapeyre.ac@gmail.com>
2016-02-19 13:58:32 -0500

    Update Algorithms.js

--- Algorithms.js
+++ Algorithms.js
@@ -1,202 +1,90 @@
-//Purpose: A file that holds the code that students fill in
-
-
-
-
-
-//Given a ray described by an initial point P0 and a direction V both in
- world coordinates, 
-//check to see 
-if it intersects the polygon described by "vertices," an array of vec3
-
-//values describing the location of the polygon vertices in its child frame.
-
-//mvMatrix is a matrix describing how to transform "vertices" into world coordinates
-
-//which you will have to do to get the correct intersection in world coordinates.
-
+//Given a ray described by an initial point P0 and a direction V both in world coordinates, check to see 
+//if it intersects the polygon described by "vertices," an array of vec3 values describing the location of 
+//the polygon vertices in its child frame.
+//mvMatrix is a matrix describing how to transform "vertices" into world coordinates which you will have to
+//do to get the correct intersection in world coordinates.
 //Be sure to compute the plane normal only after you have transformed the points,
-
 //and be sure to only compute intersections which are inside of the polygon
-
 //(you can assume that all polygons are convex and use the area method)
 
-//**PATH EXTRACTION** (may need to alter if doing transmission task later)
 function rayIntersectPolygon(P0, V, vertices, mvMatrix) {
-  
-  
 	//TODO: Fill this in
-    
-    
-
-	//Step 1: Make a new array of vec3s which holds "vertices" transformed 
-to world 
+	//Step 1: Make a new array of vec3s which holds "vertices" transformed to world 
 	//coordinates (hint: vec3 has a function "transformMat4" which is useful)
-
-
-    
-    
 	//Step 2: Compute the plane normal of the plane spanned by the transformed vertices
- 
-   
-    
 	//Step 3: Perform ray intersect plane
-  
-  
-    
-    
 	//Step 4: Check to see if the intersection point is inside of the transformed 
-	//polygon
-. You can assume that the polygon is convex.  If you use the area test, 
-	//you can
- allow for some wiggle room in the two areas you're comparing (e.g. 
-	//absolute difference
- not exceeding 1e-4)
-   
- 
-    
-    
+	//polygon. You can assume that the polygon is convex.  If you use the area test, 
+	//you can allow for some wiggle room in the two areas you're comparing (e.g. 
+	//absolute difference not exceeding 1e-4)
 	//Step 5: Return the intersection point if it exists or null if it's outside
-    
 	//of the polygon or if the ray is perpendicular to the plane normal 
 	//(no intersection)
     
-    
-
 	return {t:1e9, P:vec3.fromValues(0, 0, 0)}; //These are dummy values 
-	//you should return both an intersection point and a parameter t.  The parameter t 
-	//will be used to sort
- intersections in order of occurrence to figure out which 
-	//one happened first
+	//you should return both an intersection point and a parameter t.  The parameter t will be used to sort
+	//intersections in order of occurrence to figure out which one happened first
 
 }
 
 
-
-
-
-function addImageSourcesFunctions(scene) {
-    
-	//Setup all of the functions that students fill in that operate directly
- 
-	//on the scene
-    
+function addImageSourcesFunctions(scene) { //Setup all of the functions that operate directly on the scene
     
 
-
-	//Purpose: A recursive function provided which helps to compute intersections
-	//of rays
- with all faces in the scene, taking into consideration the scene graph
-	//structure
+	//Purpose: A recursive function provided which helps to compute intersections of rays with all faces in
+	//the scene, taking into consideration the scene graph structure
    
-
-	//Inputs: P0 (vec3): Ray starting point, V (vec3): ray direction
-    
-	//node (object): node in scene tree to process, 
-    
+	//Inputs: P0 (vec3): Ray starting point; V (vec3): ray direction; node (object): node in scene tree to process; 
 	//mvMatrix (mat4): Matrix to put geometry in this node into world coordinates
-    
-	//excludeFace: Pointer to face object to be excluded (don't intersect with
-    
-	//the face that this point lies on)
-    
-	//Returns: null if no intersection,
-    
-	//{tmin:minimum t along ray, PMin(vec3): corresponding point, faceMin:Pointer to mesh face hit first}
-    
+	//excludeFace: Pointer to face object to be excluded (don't intersect with the face that this point lies on)
+	//Returns: null if no intersection, {tmin:minimum t along ray, PMin(vec3): corresponding point, faceMin:Pointer to mesh face hit first}
     
 	//NOTE: Calling this function with node = scene and an identity matrix for mvMatrix
-    
 	//will start the recursion at the top of the scene tree in world coordinates
     
-	
-	//**PROVIDED** (may need to alter if doing transmission task later)
+	//**PROVIDED** 
 	scene.rayIntersectFaces = function(P0, V, node, mvMatrix, excludeFace) {
 	
 	var tmin = Infinity; //The parameter along the ray of the nearest intersection
-        
 		var PMin = null; //The point of intersection corresponding to the nearest interesection
-        
 		var faceMin = null;//The face object corresponding to the nearest intersection
-        
 		if (node === null) {
-            
 			return null;
-        
 		}
-        
 		if ('mesh' in node) { //Make sure it's not just a dummy transformation node
-            
 			var mesh = node.mesh;
-            
 			for (var f = 0; f < mesh.faces.length; f++) {
-                
 				if (mesh.faces[f] == excludeFace) {
-                    
 				continue; //Don't re-intersect with the face this point lies on
-                
 				}
-                
 				//Intersect the ray with this polygon
-                
 				var res = rayIntersectPolygon(P0, V, mesh.faces[f].getVerticesPos(), mvMatrix);
-                
 				if (!(res === null) && (res.t < tmin)) {
-                    
 					tmin = res.t;
-                    
 					PMin = res.P;
-                    
 					faceMin = mesh.faces[f];
-                
 				}
-            
 			}
-        
 		}
-        
-        
 		if ('children' in node) {
-            
-		//Recursively check the meshes of the children to make sure the ray
-            
-		//doesn't intersect any of them first
-            
+		//Recursively check the meshes of the children to make sure the ray doesn't intersect any of them first
 			for (var i = 0; i < node.children.length; i++) {
-                
 				var nextmvMatrix = mat4.create();
-                
-				//Multiply on the right by the next transformation of the child
- node
-                
+				//Multiply on the right by the next transformation of the childnode
 				mat4.mul(nextmvMatrix, mvMatrix, node.children[i].transform);
-                
 				//Recursively intersect with the child node
-                
 				var cres = scene.rayIntersectFaces(P0, V, node.children[i], nextmvMatrix, excludeFace);
-                
 				if (!(cres === null) && (cres.tmin < tmin)) {
-                    
 					tmin = cres.tmin;
-                    
 					PMin = cres.PMin;
-                    
 					faceMin = cres.faceMin;
-                
 				}
-            
 			}
-        
 		}
-        
 		if (PMin === null) {
-            
 			return null;
-        
 		}
-        
 		return {tmin:tmin, PMin:PMin, faceMin:faceMin};
-    
 	}
  
 
@@ -231,35 +119,20 @@ function addImageSourcesFunctions(scene) {
 
 
 	//Purpose: Based on the extracted image sources, trace back paths from the
-    
 	//receiver to the source, checking to make sure there are no occlusions
-    
 	//along the way.  Remember, you're always starting by tracing a path from
-    
 	//the receiver to the image, and then from the intersection point with
-    
 	//that image's corresponding face to the image's parent, and so on
-    
 	//all the way until you get back to the original source.
-    
-    
+
 	//Fill in the array scene.paths, where each element of the array is itself
-    
 	//an array of objects describing vertices along the path, starting
-    
 	//with the receiver and ending with the source.  Each object in each path
-    
 	//array should contain a field "pos" which describes the position, as well
-    
 	//as an element "rcoeff" which stores the reflection coefficient at that
-    
 	//part of the path, which will be used to compute decays in "computeInpulseResponse()"
-    
 	//Don't forget the direct path from source to receiver!
-    
 
-
-	/**PATH EXTRACTION**
 	scene.extractPaths = function() {
         
 		scene.paths = [];
@@ -281,15 +154,9 @@ function addImageSourcesFunctions(scene) {
     
 	}
 
-
-    
-    
     
 	//Inputs: Fs: Sampling rate (samples per second)
-    
 	
-
-	//**IMPULSE RESPONSE GENERATION**
 	scene.computeImpulseResponse = function(Fs) {
         
 		var SVel = 340;	//Sound travels at 340 meters/second
